dc12632a6aa3d61395bd616e681fbab0c5e24e23
kamenco <85615294+kamenco@users.noreply.github.com>
2024-05-21 17:27:44 +0100

    In the code with curly brackets the space between the% and} war deleted.

--- run.py
+++ run.py
@@ -1,27 +1,23 @@
 import os
 from flask import Flask, render_template
 
-
 app = Flask(__name__)
-
 
 @app.route("/")
 def index():
-    return render_template("index.html")
-
+    return render_template("index.html", page_title="Home")
 
 @app.route("/about")
 def about():
     return render_template("menu.html", page_title="Menu")
 
-
 @app.route("/contact")
 def contact():
     return render_template("contact.html", page_title="Contact")
-
 
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
         port=int(os.environ.get("PORT", "5000")),
-        debug=True)
+        debug=True
+    )
