52463acad1a236904375f64df04b455053793da2
kamenco <kamen@bgtown.com>
2024-06-18 15:31:10 +0000

    Fixed template not found issue

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='taskmanager/templates')
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "supersecretkey")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL", "sqlite:///taskmanager.db")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
