457cf14b86854cb22836707c9aef3295b4936ce3
kamenco <kamen@bgtown.com>
2024-06-20 18:38:52 +0000

    The __init__.py file was edited now showing the path of the ststic folder static_folder=taskmanager/static

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__, template_folder='taskmanager/templates')
+app = Flask(__name__, static_folder='taskmanager/static', template_folder='taskmanager/templates')
+
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY", "supersecretkey")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL", "sqlite:///taskmanager.db")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
